f97764414dc8090fea644504627057558c2d2e4b
Eduardo Messias <messiasgit@outlook.com>
2022-08-21 13:37:34 -0300

    feat: json-server

--- src/components/vendas/Leads.vue
+++ src/components/vendas/Leads.vue
@@ -1,3 +1,29 @@
 <template>
     <h5>Leads</h5>
+    {{dados}}
 </template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    name: 'Leads',
+    data(){
+        return{
+            dados: null,
+        }
+    },
+    methods:{
+        getDadosApi: async function(){
+            let response = await axios.get('http://localhost:3000/leads')
+            let payload = response.data
+            this.dados = payload
+            console.log(payload)
+            
+        }
+    },
+    created(){
+        this.getDadosApi()
+    }
+}
+</script>
